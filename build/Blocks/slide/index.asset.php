f7c56cca05e5e2999799edee79f3c93fee784471
Lara van Diemen <lara@yard.nl>
2024-03-12 14:34:12 +0100

    (chore): npm run build

---
+++ build/Blocks/slide/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-hooks'), 'version' => 'f65e6fed43265d34866d');
---
+++ build/Blocks/slider/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => '78b00b1b02557cb87e11');
