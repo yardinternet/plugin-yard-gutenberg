6fa6fe9671738ff6592dedc9f8ba20926d18c694
Lara van Diemen <lara@yard.nl>
2024-03-18 09:48:02 +0100

    (feat): add taxonomy select control

--- src/Blocks/query/components/filters-controls/taxonomy-control.js
+++ src/Blocks/query/components/filters-controls/taxonomy-control.js
@@ -8,11 +8,12 @@ import { useEffect, useState } from '@wordpress/element';
  * Internal dependencies
  */
 import TaxonomyToggleControl from './taxonomy-toggle-control';
+import TaxonomySelectControl from './taxonomy-select-control';
 import { fetchTaxonomiesByPostType } from '../../utils/api';
 import { filterTaxonomies } from '../../utils/taxonomies';
 
 const TaxonomyControl = ( props ) => {
-	const { query, removeParameter, setAttributes, attributes } = props;
+	const { query, removeParameter, attributes } = props;
 	const { enableTaxonomies, enableManualSelection } = attributes;
 	const [ taxonomies, setTaxonomies ] = useState( [] );
 
@@ -22,13 +23,17 @@ const TaxonomyControl = ( props ) => {
 
 	/**
 	 * Fetch taxonomies of selected post types
-	 *
-	 * @todo Make it possible to get taxonomies for multiple post types
 	 */
 	const getTaxonomies = async () => {
-		const allTaxonomies = await fetchTaxonomiesByPostType(
-			query.post_type.join( ',' )
-		);
+		let allTaxonomies = {};
+
+		for ( const key in query.post_type ) {
+			const typeTaxonomies = await fetchTaxonomiesByPostType(
+				query.post_type[ key ]
+			);
+			allTaxonomies = { ...allTaxonomies, ...typeTaxonomies };
+		}
+
 		const filteredTaxonomies = filterTaxonomies( allTaxonomies );
 
 		setTaxonomies( filteredTaxonomies );
@@ -45,9 +50,14 @@ const TaxonomyControl = ( props ) => {
 
 				{ enableTaxonomies &&
 					taxonomies.map( ( taxonomy ) => {
-						// @todo Include taxonomy terms
-						// @todo Exclude taxonomy terms
-						return <p key={ taxonomy.slug }>{ taxonomy.name }</p>;
+						return (
+							<div key={ taxonomy.slug }>
+								<TaxonomySelectControl
+									taxonomy={ taxonomy }
+									{ ...props }
+								/>
+							</div>
+						);
 					} ) }
 			</>
 		)
