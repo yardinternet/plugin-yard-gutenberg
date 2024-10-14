9bf0f031c710476be882ef0d362fa67f1d09006f
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2024-10-14 17:17:21 +0200

    (feat) retrieve current facets and template config before render

--- src/Blocks/facetwp/Facetwp.php
+++ src/Blocks/facetwp/Facetwp.php
@@ -8,6 +8,14 @@ class Facetwp
 {
 	public function renderCallback(array $attributes)
 	{
+		if (function_exists('FWP')) {
+			$attributes['selectedFacets'] = array_map(
+				fn (array $facet) => FWP()->helper->get_facet_by_name($facet['name']),
+				$attributes['selectedFacets']
+			);
+			$attributes['selectedTemplate'] = FWP()->helper->get_template_by_name($attributes['selectedTemplate']['name']);
+		}
+
 		return view('blocks.FacetWP.index', [
 			'template' => $attributes['selectedTemplate'],
 			'facets' => $attributes['selectedFacets'],
