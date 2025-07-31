8c309a0157ced16fa265ba025c691c169b63d33f
Wybe van den Bosch <wybe.vandenbosch+git@gmail.com>
2025-07-31 16:37:48 +0200

    (chore): npm run build

--- build/Blocks/collapse-item/index.asset.php
+++ build/Blocks/collapse-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices'), 'version' => '3b75a1c5f7cc3ba88793');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices'), 'version' => '21ece3e574e6553248bb');
