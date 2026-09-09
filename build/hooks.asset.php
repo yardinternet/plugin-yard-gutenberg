de48f8400d2e5b5015e472512cc5166db37edf19
YvetteNikolov <yvettenikolov@gmail.com>
2026-09-09 16:20:35 +0200

    feat: add minimumColumnWidth attribute to default grid block

--- build/hooks.asset.php
+++ build/hooks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-hooks', 'wp-primitives'), 'version' => 'cfa7e7c039644e6af3ca');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-hooks', 'wp-primitives'), 'version' => '4b43d364c7c1886cac64');
